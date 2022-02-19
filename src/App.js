aac5c7054581e2aad133e914dd8a47689639dffb
diamond-nicholas <diamondnicholas154@gmail.com>
2022-02-19 08:35:49 +0100

    made it better

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.scss';
 
 function App() {
-  return <ChatEngine height='100vh' projectID='' userName='' userSecret='' />;
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID='c22d2956-c98c-46fd-906a-fca865ff7420'
+      userName='DiamondNicholas'
+      userSecret='4Youand4me$'
+    />
+  );
 }
 
 export default App;
